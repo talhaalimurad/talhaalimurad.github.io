baea1ede7508e96229baabb6590fbe85f3ad42ff
talhaalimurad <talhamurad11@gmail.com>
2023-09-21 15:16:49 -0600

    created a moving shape

--- 02 Primitive Paint/sketch.js
+++ 02 Primitive Paint/sketch.js
@@ -1,7 +1,10 @@
 // Primitive Paint
 // Talha Ali Murad
 // 19-09-2023
-//
+
+// global variables
+let circleSize = 20;
+let overlay;
 
 
 function setup() {
@@ -10,4 +13,21 @@ function setup() {
 
 function draw() {
   background(220);
+  drawAndMoveCircle();
+  mouseShape();
+}
+
+function drawAndMoveCircle(){
+  circle(windowWidth/2,windowHeight/2,circleSize);
+  fill("lightgreen");
+  circleSize = circleSize += 1;
+  if(circleSize >= 150){
+    circleSize = 20;
+  }
+}
+
+function mouseShape(){
+  if(mouseIsPressed){
+    rect(mouseX,mouseY,30,20);
+  }
 }
