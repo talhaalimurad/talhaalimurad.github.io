f2236e2e7cbb0ea7edd4fb2182d2542543bc0a99
talhaalimurad <talhamurad11@gmail.com>
2023-09-27 14:20:42 -0600

    created a new project file

---
+++ 07 Multi-Coloured Grid/sketch.js
@@ -0,0 +1,16 @@
+// Multi-Coloured Grid
+// Talha Ali Murad
+// 27-09-2023
+//
+// Creating a random multicoloured grid that increases and decreases in
+// size with each mouse click
+
+// global variables
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
