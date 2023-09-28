22d36e6ccc694c610f532606ff9b10c22f4b63cd
talhaalimurad <talhamurad11@gmail.com>
2023-09-28 13:04:52 -0600

    added functions

--- 07 Multi-Coloured Grid/sketch.js
+++ 07 Multi-Coloured Grid/sketch.js
@@ -6,11 +6,31 @@
 // size with each mouse click
 
 // global variables
+let b;
+let squareSize = 25;
+let gridSpacing = 25;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  drawGrid();
+}
+
+function drawGrid(){
+  // draws multi coloured squares covering the entire canvas
+  for(let x = 0; x < width; x = x + gridSpacing){
+    for(let y = 0; y  < height; y = y + gridSpacing){
+      b = random(100, 250,);
+      fill(20,60,b);
+      square(x, y, squareSize);
+    }
+  }
+}
+
+function mousePressed(){
+  drawGrid();
+  squareSize = squareSize += 5;
+  gridSpacing = gridSpacing += 5;
 }
 
 function draw() {
-  background(220);
 }
