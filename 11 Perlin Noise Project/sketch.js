bfd7772b89de9a82fa3617b8105b698264be0c23
talhaalimurad <145162380+talhaalimurad@users.noreply.github.com>
2023-10-12 15:28:23 -0600

    added panning and flag

--- 11 Perlin Noise Project/sketch.js
+++ 11 Perlin Noise Project/sketch.js
@@ -5,41 +5,44 @@
 // Generating random terrain using noise()
 
 // global variables
-let rectWidth = 3;
-let rectHeightTime = 20;
-let noiseShift = 0.01;
+let rectWidth = 2;
+let rectHeightTime = 0;
+let noiseShift = 0.005;
 let peakX, peakY;
+let globalNoise=0;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
   peakY = height;
-  strokeWeight(4);
-  generateTerrain();
-  drawFlag(peakX, peakY);
 }
 
-function generateTerrain(){
+function generateTerrain(noiseValue){
   // drawing rectangles of random heights using a loop
+  globalNoise += noiseShift;
+  peakY = height;
   for(let x = 0; x < width; x += rectWidth){
+    noiseValue += noiseShift;
     rectMode(CORNERS);
-    let rectHeight = noise(rectHeightTime);
-    rectHeight = map(rectHeight, 0, 1, 5, height);
-    if(rectHeight < peakY){
-      peakY = rectHeight;
-      peakX = x;
-    }
-    rectHeightTime += noiseShift;
+    let rectHeight = noise(noiseValue) * height;
     fill(0);
     rect(x , height, x + rectWidth, height - rectHeight);
+    if(peakY > height - rectHeight){
+      peakX = x + rectWidth;
+      peakY = height - rectHeight;
+    }
   }
 }
 
 function drawFlag(x, y){
-  line(x, y, x, y - 30);
-  triangle(x, y - 30, x, y - 20, x + 10, y - 25);
+  fill(255, 0, 0);
+  rect(x - 2, y, x + 2, y - 30);
+  triangle(x - 2, y - 40, x - 2, y - 20, x + 15, y - 30);
 }
 
 function draw() {
-  // background(220);
+  background(220);
+  generateTerrain(globalNoise);
+  drawFlag(peakX, peakY);
 }
