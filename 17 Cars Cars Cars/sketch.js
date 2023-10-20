ee23673e402f01e9d4746e93dc11dc0d71da650f
talhaalimurad <145162380+talhaalimurad@users.noreply.github.com>
2023-10-20 15:35:45 -0600

    created new functions

--- 17 Cars Cars Cars/sketch.js
+++ 17 Cars Cars Cars/sketch.js
@@ -1,10 +1,11 @@
-// Project Title
-// Your Name
-// Date
+// Cars Cars Cars
+// Talha Ali Murad
+// 19-10-2023
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// 
 
+//  global variables
+let segW = 25;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +13,20 @@ function setup() {
 
 function draw() {
   background(220);
+  drawRoad();
+}
+
+function drawRoad(){
+  // draws a road with a dotted line through the centre
+  fill(0);
+  stroke(0);
+  rect(0, height/3, width, height/3);
+
+  let x = 0;
+  while(x < width){
+    stroke(255);
+    strokeWeight(5);
+    line(x, height/2, x + 8, height/2);
+    x += segW;
+  }
 }
