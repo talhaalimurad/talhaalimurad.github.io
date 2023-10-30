7aabdba12cd329f76cdcf0779446d8123125fe24
talhaalimurad <145162380+talhaalimurad@users.noreply.github.com>
2023-10-30 15:23:58 -0600

    updated code

--- 17 Cars Cars Cars/sketch.js
+++ 17 Cars Cars Cars/sketch.js
@@ -6,21 +6,24 @@
 
 //  global variables
 let segW = 25;
+let myCar;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  myCar = new Vehicle(width/2, height/3);
 }
 
 function draw() {
   background(220);
   drawRoad();
+  myCar.display();
 }
 
 function drawRoad(){
   // draws a road with a dotted line through the centre
   fill(0);
   stroke(0);
-  rect(0, height/3, width, height/3);
+  rect(0, height/4, width, height/2);
 
   let x = 0;
   while(x < width){
@@ -28,5 +31,52 @@ function drawRoad(){
     strokeWeight(5);
     line(x, height/2, x + 8, height/2);
     x += segW;
+  }
+}
+
+class Vehicle{
+  constructor(x, y){
+    this.choice = Math.floor(random(0, 1));
+    this.direction = Math.floor(random(0, 1));
+    this.xSpeed = 5;
+    this.color = [random(255), random(255), random(255)];
+    this.x = x;
+    this.y = y;
+  }
+
+  display(){
+    noStroke();
+    fill(this.color[0], this.color[1], this.color[2]);
+    rect(this.x, this.y, 25, 15);
+  }
+
+  move(){
+    if(this.direction===1){
+      if(this.x < width){
+        this.x += this.xSpeed;
+      }
+    }
+    else if(this.direction===0){
+      if(this.x < width){
+        this.x -= this.xSpeed;
+      }
+    }
+  }
+
+  speedUp(){
+    if(this.xSpeed < 15){
+      this.xSpeed++;
+    }
+  }
+
+  speedDown(){
+    this.xSpeed -= 15;
+  }
+
+  changeColor(){
+  }
+
+  action(){
+
   }
 }
