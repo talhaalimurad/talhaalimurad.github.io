11fd6b8760d1b68784ae383c0c5afdec7f4c2960
talhaalimurad <talhamurad11@gmail.com>
2023-11-16 13:01:12 -0600

    updated code

--- 17 Cars Cars Cars/sketch.js
+++ 17 Cars Cars Cars/sketch.js
@@ -2,7 +2,6 @@
 // Talha Ali Murad
 // 19-10-2023
 //
-// 
 
 //  global variables
 let segL = 25;
@@ -13,134 +12,124 @@ let westbound = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  for(let numVehicles = 20; numVehicles > 0; numVehicles--){
+  for (let numVehicles = 20; numVehicles > 0; numVehicles--) {
     let d = Math.floor(random(0, 2));
-    if(d===1) eastbound.push(new Vehicle(random(0, width), d));
-    if(d===0) westbound.push(new Vehicle(random(0, width), d));
+    if (d === 1) eastbound.push(new Vehicle(random(0, width), d));
+    if (d === 0) westbound.push(new Vehicle(random(0, width), d));
   }
 }
 
 function draw() {
   background(255);
   drawRoad();
-  for(let car of eastbound){
+  for (let car of eastbound) {
     car.display();
     car.move();
   }
-  for(let car of westbound){
+  for (let car of westbound) {
     car.display();
     car.move();
   }
 }
 
-function drawRoad(){
+function drawRoad() {
   // draws a road with a dotted line through the centre
   fill(0);
   stroke(0);
-  rect(0, height/4, width, height/2);
+  rect(0, height / 4, width, height / 2);
 
   let x = 0;
-  while(x < width){
+  while (x < width) {
     stroke(255);
     strokeWeight(5);
-    line(x, height/2, x + 8, height/2);
+    line(x, height / 2, x + 8, height / 2);
     x += segL;
   }
 }
 
-class Vehicle{
-  constructor(x, d){
+class Vehicle {
+  constructor(x, y, d) {
     this.vehicleChoice = Math.floor(random(0, 2));
     this.direction = d;
     this.speedChance = Math.floor(random(0, 100));
-    this.color = [random(255), random(255), random(255)];
+    this.color = color(random(255), random(255), random(255));
     this.x = x;
-    this.yEast = random(height/4, height/2 - 20);
-    this.yWest =  random(height/2 + 5, height - height/4 - 15);
+    this.y = y;
+    this.xSpeed = 5;
+    this.maxSpeed = 10;
+
+    // y values for the top and bottom
+    // random(height / 4, height / 2 - 20);
+    // random(height / 2 + 5, height - height / 4 - 15);
+    
+    // random speed values
     // this.xTime = random(10);
     // this.timeShift = 0.01;
     // this.maxSpeed = random(5);
-    this.xSpeed = 5;
-    this.maxSpeed = 10;
   }
 
-  display(){
+  display() {
     noStroke();
     if(this.direction===1){
       if(this.vehicleChoice===1){
-        fill(this.color[0], this.color[1], this.color[2]);
-        rect(this.x, this.yEast, 30, 15);
-        fill(200, 230, 255);
-        rect(this.x + 19.3, this.yEast - 4, 10, 5);
-        rect(this.x, this.yEast - 4, 10, 5);
-        rect(this.x + 19.3, this.yEast + 15, 10, 5);
-        rect(this.x, this.yEast + 15, 10, 5);
+        this.drawCar();
       }
-      else if(this.vehicleChoice===0){
-        fill(this.color[0], this.color[1], this.color[2]);
-        rect(this.x, this.yEast, 30, 20);
-        fill(200, 230, 255);
-        rect(this.x + 10, this.yEast, 10, 19.5);
-      }
-    }
-    else{
-      if(this.vehicleChoice===1){
-        fill(this.color[0], this.color[1], this.color[2]);
-        rect(this.x, this.yWest, 30, 15);
-        fill(200, 230, 255);
-        rect(this.x + 19.3, this.yWest - 4, 10, 5);
-        rect(this.x, this.yWest - 4, 10, 5);
-        rect(this.x + 19.3, this.yWest + 15, 10, 5);
-        rect(this.x, this.yWest + 15, 10, 5);
-      }
-      else if(this.vehicleChoice===0){
-        fill(this.color[0], this.color[1], this.color[2]);
-        rect(this.x, this.yWest, 30, 20);
-        fill(200, 230, 255);
-        rect(this.x + 10, this.yWest, 10, 19.5);
+      else{
+        this.drawCarTwo();
       }
     }
   }
 
-  move(){
-    if(this.direction===1){
+  drawCar() {
+    fill(this.color);
+    rect(this.x, this.y, 30, 15);
+    fill(200, 230, 255);
+    rect(this.x + 19.3, this.y - 4, 10, 5);
+    rect(this.x, this.y - 4, 10, 5);
+    rect(this.x + 19.3, this.y + 15, 10, 5);
+    rect(this.x, this.y + 15, 10, 5);
+  }
+
+  drawCarTwo(){
+    fill(this.color);
+    rect(this.x, this.y, 30, 20);
+    fill(200, 230, 255);
+    rect(this.x + 10, this.y, 10, 19.5);
+  }
+
+  move() {
+    if (this.direction === 1) {
+      this.x += this.xSpeed;
+
+      // speed using random speed values
       // let xSpeed = noise(this.xTime);
       // xSpeed = map(xSpeed, 0, 1, this.maxSpeed, this.maxSpeed);
       // this.xTime += this.timeShift;
-      this.x += this.xSpeed;
     }
 
-    else if(this.direction===0){
+    else if (this.direction === 0) {
       this.x -= this.xSpeed;
     }
 
-    if(this.x < 0) this.x += width;
-    if(this.x > width) this.x -= width;
+    if (this.x < 0) this.x += width;
+    if (this.x > width) this.x -= width;
   }
 
-  speedUp(){
-    for(let s = 0; s < this.maxSpeed; s++){
+  speedUp() {
+    for (let s = 0; s < this.maxSpeed; s++) {
       this.xSpeed += 5;
     }
   }
-  
-  speedDown(){
-    for(let s = this.xSpeed; s > 2; s--){
+
+  speedDown() {
+    for (let s = this.xSpeed; s > 2; s--) {
       this.xSpeed -= 5;
     }
   }
 
-  changeColor(){
+  changeColor() {
   }
-  action(){
-    this.display();
-    this.move();
-    if(this.speedChance===1){
-      this.speedUp();
-    }
-    else if(this.speedChance===0){
-      this.speedDown();
-    }
-    this.changeColor();
+
+  action() {
   }
 }
