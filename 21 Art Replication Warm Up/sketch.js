9cbfdb0ed337978b535ec2ef8eaf1cf9f967d2da
talhaalimurad <talhamurad11@gmail.com>
2023-12-07 13:05:32 -0600

    updated code

--- 21 Art Replication Warm Up/sketch.js
+++ 21 Art Replication Warm Up/sketch.js
@@ -4,27 +4,37 @@
 // Ninety Parallel Sinusoids With Linearly Increasing Period
 
 // Global Variables
-let period = 0.01;
-let peak = 50;
-let trough = 100;
+let period = 0.1;
+let peak = 100;
+let trough = 180;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
   drawSinusiod();
+  drawSinusiods();
 }
 
 function draw() {
   //background(220);
 }
 
+function drawSinusiods(){
+  for(let i = 120; i >= 0; i--){
+    period = 0.1;
+    peak += 4;
+    trough += 4;
+    drawSinusiod();
+  }
+}
+
 function drawSinusiod(){
   for(let x = 0; x < width; x++){
-    let y = sin(x * period * 0.9);
+    let y = sin(x * period * 0.8);
     y = map(y, -1, 1, peak, trough);
-    if(x > width/8 && x < width * 0.77){
+    if(x > width/8 && x < width * 0.66){
       point(x, y);
-      period += 0.00298;
+      period += 0.0038;
     }
   }
 }
