daba1933259ebe006fe7f13530b09947e7177caf
talhaalimurad <talhamurad11@gmail.com>
2023-12-12 15:23:02 -0600

    Added Images
    Added the images for the main characters design to an assets folder.

---
+++ 34 Final Project/sketch.js
@@ -0,0 +1,13 @@
+// Final Project
+// Talha Ali Murad
+// 12-12-2023
+// Monkey Mart Replica Game
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
