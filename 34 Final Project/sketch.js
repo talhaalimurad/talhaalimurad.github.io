0edd2e0547fd6279abd39da90e0b9e5682d969d1
talhaalimurad <talhamurad11@gmail.com>
2023-12-13 15:20:23 -0600

    Customer Class
    Created a customer class that creates a customer that moves randomly. More work needs to be done to make sure that a random direction is not chosen each frame.

--- 34 Final Project/sketch.js
+++ 34 Final Project/sketch.js
@@ -6,24 +6,33 @@
 // global variables
 let playerLeft;
 let playerRight;
+let customerOneLeft;
+let customerOneRight;
 let facingRight = true;
+let myCustomer;
+this.walkTime = 60;
 let playerX;
 let playerY;
 
 function preload(){
   playerLeft = loadImage("assets/Player Left.png");
   playerRight = loadImage("assets/Player Right.png");
+  customerOneLeft = loadImage("assets/CustomerOne Right.png");
+  customerOneRight = loadImage("assets/CustomerOne Right.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   playerX = width/2;
   playerY = height/2;
+  myCustomer = new Customer(width/2, height/2);
 }
 
 function draw() {
   background(243, 238, 156);
   drawPlayer(playerX, playerY);
+  myCustomer.draw();
+  myCustomer.move();
 }
 
 function drawPlayer(x, y){
@@ -41,5 +50,33 @@ function drawPlayer(x, y){
     facingRight = false;
   }
   if(keyIsDown(DOWN_ARROW)) playerY += 5;
-  if(keyIsDown(UP_ARROW)) playerY -= 5; 
+  if(keyIsDown(UP_ARROW)) playerY -= 5;
+}
+
+class Customer{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.type;
+    this.customers = [];
+    this.facingRight = true;
+    this.direction = floor(random(0, 4));
+  }
+
+  draw(){
+    if(this.facingRight===true) image(customerOneRight, this.x, this.y, 55, 55);
+
+  }
+
+  move(){
+    this.direction = floor(random(0, 4));
+    if(this.x >= 10 && this.x <= width){
+      if(this.y >= 10 && this.y <= height){
+        if(this.direction===0) this.x += 4;
+        if(this.direction===1) this.x -= 4;
+        if(this.direction===2) this.y += 4;
+        if(this.direction===3) this.y -= 4;
+      }
+    }
+  }
 }
